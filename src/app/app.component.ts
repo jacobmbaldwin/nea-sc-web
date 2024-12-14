6be8436626117a5d511897b211cc90fdbf507574
Jacob <jacobmbaldwin@gmail.com>
2024-12-14 09:44:23 -0600

    Begin adding header and navigation components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from "./components/navigation-bar/navigation-bar.component";
+import { HeaderBarComponent } from "./components/header-bar/header-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationBarComponent, HeaderBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
