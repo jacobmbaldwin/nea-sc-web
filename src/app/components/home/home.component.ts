ae824759aeecc0a40124e33a664e1ab66b28e226
Jacob <jacobmbaldwin@gmail.com>
2025-03-19 12:16:23 -0500

    Added sponsor section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { OfferingCardsComponent } from "./offering-cards/offering-cards.component";
 import { HeroComponent } from "./hero/hero.component";
 import { GenericBannerComponent } from "./generic-banner/generic-banner.component";
+import { SponsorSectionComponent } from "./sponsor-section/sponsor-section.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [OfferingCardsComponent, HeroComponent, GenericBannerComponent],
+  imports: [OfferingCardsComponent, HeroComponent, GenericBannerComponent, SponsorSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/components/home/sponsor-section/sponsor-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sponsor-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './sponsor-section.component.html',
+  styleUrl: './sponsor-section.component.css'
+})
+export class SponsorSectionComponent {
+
+}
