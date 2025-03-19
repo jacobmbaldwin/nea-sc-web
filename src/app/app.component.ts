ae9f4510b2bd8c741ab197890bcad9740166805d
Jacob <jacobmbaldwin@gmail.com>
2025-03-19 12:34:11 -0500

    Add footer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from "./components/navigation-bar/navigation-bar.component";
 import { ReactiveFormsModule } from '@angular/forms';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationBarComponent, ReactiveFormsModule],
+  imports: [RouterOutlet, NavigationBarComponent, ReactiveFormsModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
