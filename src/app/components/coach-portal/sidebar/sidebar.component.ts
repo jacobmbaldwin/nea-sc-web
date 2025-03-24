81639bca3a2be97a72c1558e794b40e8207a1fee
jabaldwin <jabaldwin@basspro.com>
2025-03-24 14:40:45 -0500

    Coach's Portal Updates

--- src/app/components/coach-portal/sidebar/sidebar.component.ts
+++ src/app/components/coach-portal/sidebar/sidebar.component.ts
@@ -14,24 +14,55 @@ export class SidebarComponent {
 
   @Output() isActiveChange: EventEmitter<string> = new EventEmitter<string>();
 
+  addEventIsActive: boolean = false;
+  scholarshipRequestsIsActive: boolean = false;
+  viewDocumentsIsActive: boolean = false;
+  viewRostersIsActive: boolean = false;
+  clubContactsIsActive: boolean = false;
+
   protected onAddEvent(): void {
     this.isActiveChange.emit('addEvent');
+    this.addEventIsActive = true;
+    this.scholarshipRequestsIsActive = false;
+    this.clubContactsIsActive = false;
+    this.viewDocumentsIsActive = false;
+    this.viewRostersIsActive = false;
   }
 
   protected onScholarshipRequests(): void {
     this.isActiveChange.emit('scholarshipRequests');
+    this.scholarshipRequestsIsActive = true;
+    this.addEventIsActive = false;
+    this.clubContactsIsActive = false;
+    this.viewDocumentsIsActive = false;
+    this.viewRostersIsActive = false;
   }
 
   protected onViewDocuments(): void {
     this.isActiveChange.emit('viewDocuments');
+    this.viewDocumentsIsActive = true;
+    this.addEventIsActive = false;
+    this.scholarshipRequestsIsActive = false;
+    this.clubContactsIsActive = false;
+    this.viewRostersIsActive = false;
   }
 
   protected onViewRosters(): void {
     this.isActiveChange.emit('viewRosters');
+    this.viewRostersIsActive = true;
+    this.addEventIsActive = false;
+    this.scholarshipRequestsIsActive = false;
+    this.clubContactsIsActive = false;
+    this.viewDocumentsIsActive = false
   }
 
-  onClubContacts(): void {
+  protected onClubContacts(): void {
     this.isActiveChange.emit('clubContacts');
+    this.clubContactsIsActive = true;
+    this.addEventIsActive = false;
+    this.scholarshipRequestsIsActive = false;
+    this.viewDocumentsIsActive = false;
+    this.viewRostersIsActive = false;
   }
 
 }
