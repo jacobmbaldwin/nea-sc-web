bd4f6afbad94a42fb85cf956de900523e8834cb6
Jacob <jacobmbaldwin@gmail.com>
2025-03-24 06:15:47 -0500

    Added coach portal functionality

--- src/app/components/coach-portal/coach-portal.component.ts
+++ src/app/components/coach-portal/coach-portal.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AddEventComponent } from "./add-event/add-event.component";
 import { SmallHeaderBannerComponent } from '../shared/small-header-banner/small-header-banner.component';
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @Component({
   selector: 'app-coach-portal',
   standalone: true,
-  imports: [AddEventComponent, SmallHeaderBannerComponent],
+  imports: [AddEventComponent, SmallHeaderBannerComponent, SidebarComponent],
   templateUrl: './coach-portal.component.html',
   styleUrl: './coach-portal.component.css'
 })
---
+++ src/app/components/coach-portal/sidebar/sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    MatIconModule
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+
+}
